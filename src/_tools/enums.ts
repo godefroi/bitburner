f4fcfe6ac96d5537d1addebcc377b9d6739bf7c9
Mark Parker <mparker@medicity.com>
2023-04-24 06:37:42 -0600

    Add crime enum, freeze everything

--- src/_tools/enums.ts
+++ src/_tools/enums.ts
@@ -1,4 +1,4 @@
-import { NS, type CityName } from "@ns";
+import { NS, type CityName, type CrimeType } from "@ns";
 
 export const CITIES = Object.freeze({
 	"Aevum":     "Aevum"      as CityName,
@@ -21,26 +21,59 @@ export const CITIES = Object.freeze({
 });
 
 
+export const CRIMES = Object.freeze({
+	shoplift:       "Shoplift"         as CrimeType,
+	robStore:       "Rob Store"        as CrimeType,
+	mug:            "Mug"              as CrimeType,
+	larceny:        "Larceny"          as CrimeType,
+	dealDrugs:      "Deal Drugs"       as CrimeType,
+	bondForgery:    "Bond Forgery"     as CrimeType,
+	traffickArms:   "Traffick Arms"    as CrimeType,
+	homicide:       "Homicide"         as CrimeType,
+	grandTheftAuto: "Grand Theft Auto" as CrimeType,
+	kidnap:         "Kidnap"           as CrimeType,
+	assassination:  "Assassination"    as CrimeType,
+	heist:          "Heist"            as CrimeType,
+
+	length: 12,
+
+	[Symbol.iterator]: function*() {
+		yield CRIMES.shoplift;
+		yield CRIMES.robStore;
+		yield CRIMES.mug;
+		yield CRIMES.larceny;
+		yield CRIMES.dealDrugs;
+		yield CRIMES.bondForgery;
+		yield CRIMES.traffickArms;
+		yield CRIMES.homicide;
+		yield CRIMES.grandTheftAuto;
+		yield CRIMES.kidnap;
+		yield CRIMES.assassination;
+		yield CRIMES.heist;
+	}
+});
+
+
 /** Universities, in order of "bestness" */
-export const UNIVERSITIES = [
+export const UNIVERSITIES = Object.freeze([
 	{ city: CITIES.Volhaven, university: "Rothman University"         },
 	{ city: CITIES.Aevum,    university: "Summit University"          },
 	{ city: CITIES.Sector12, university: "ZB Institute of Technology" },
-];
+]);
 
 
 /** Gyms, in order of "bestness" */
-export const GYMS = [
+export const GYMS = Object.freeze([
 	{ city: CITIES.Aevum,    gym: "Snap Fitness Gym"      },
 	{ city: CITIES.Volhaven, gym: "Millenium Fitness Gym" },
 	{ city: CITIES.Sector12, gym: "Powerhouse Gym"        },
-];
+]);
 
 
 /** Combat skills */
-export const COMBAT_SKILLS = [
+export const COMBAT_SKILLS = Object.freeze([
 	{ stat: "Strength",  currentLevel: (ns: NS) => ns.getPlayer().skills.strength  },
 	{ stat: "Agility",   currentLevel: (ns: NS) => ns.getPlayer().skills.agility   },
 	{ stat: "Defense",   currentLevel: (ns: NS) => ns.getPlayer().skills.defense   },
 	{ stat: "Dexterity", currentLevel: (ns: NS) => ns.getPlayer().skills.dexterity },
-];
+]);

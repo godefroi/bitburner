829522500e11391d62666ebef39e5583e8634b97
Mark Parker <mparker@medicity.com>
2023-04-27 06:12:43 -0600

    fill out faction info

--- src/_tools/faction.ts
+++ src/_tools/faction.ts
@@ -1,15 +1,68 @@
 import { type CityName, NS } from "@ns";
 import { CITIES } from "@/_tools/enums";
 
-export interface Faction {
-	readonly name: string
-	readonly type: FactionType,
-	readonly excludes: readonly string[],
-	readonly corpCity: (CityName | undefined),
-	readonly backdoorServer: (string | undefined),
-	readonly hackingLevel: number,
-	readonly cashOnHand: number,
-	//readonly requirements: readonly Requirement[],
+interface FactionOptions {
+	name: string,
+	type: FactionType,
+	excludes?: string[],
+	city?: CityName | undefined,
+	backdoorServer?: string | undefined,
+	hackingLevel?: number,
+	cashOnHand?: number,
+	peopleKilled?: number,
+	karma?: number,
+	hacknetLevels?: number,
+	hacknetRam?: number,
+	hacknetCores?: number,
+	infiltration?: boolean,
+	combatStats?: number,
+	corpExec?: boolean,
+	notGovJob?: boolean,
+	augmentations?: number,
+	hackLevelOrCombat?: boolean,
+}
+
+
+export class Faction {
+	name:              string;
+	type:              FactionType;
+	excludes:          readonly string[];
+	city:              (CityName | undefined);
+	backdoorServer:    (string | undefined);
+	hackingLevel:      number;
+	cashOnHand:        number;
+	peopleKilled:      number;
+	karma:             number;
+	hacknetLevels:     number;
+	hacknetRam:        number;
+	hacknetCores:      number;
+	infiltration:      boolean;
+	combatStats:       number;
+	corpExec:          boolean;
+	notGovJob:         boolean;
+	augmentations:     number;
+	hackLevelOrCombat: boolean;
+
+	constructor(opts: FactionOptions) {
+		this.name              = opts.name;
+		this.type              = opts.type;
+		this.excludes          = opts.excludes ?? [];
+		this.city              = opts.city;
+		this.backdoorServer    = opts.backdoorServer;
+		this.hackingLevel      = opts.hackingLevel ?? 0;
+		this.cashOnHand        = opts.cashOnHand ?? 0;
+		this.peopleKilled      = opts.peopleKilled ?? 0;
+		this.karma             = opts.karma ?? 0;
+		this.hacknetLevels     = opts.hacknetLevels ?? 0;
+		this.hacknetRam        = opts.hacknetRam ?? 0;
+		this.hacknetCores      = opts.hacknetCores ?? 0;
+		this.infiltration      = opts.infiltration ?? false;
+		this.combatStats       = opts.combatStats ?? 0;
+		this.corpExec          = opts.corpExec ?? false;
+		this.notGovJob         = opts.notGovJob ?? false;
+		this.augmentations     = opts.augmentations ?? 0;
+		this.hackLevelOrCombat = opts.hackLevelOrCombat ?? false;
+	}
 }
 
 export enum FactionType {
@@ -59,67 +112,45 @@ export const FACTION_NAMES = {
 	ShadowsOfAnarchy: "Shadows of Anarchy"
 };
 
-interface FactionOptions {
-	name: string,
-	type: FactionType,
-	excludes?: string[],
-	corpCity?: CityName | undefined,
-	backdoorServer?: string | undefined,
-	hackingLevel?: number,
-	cashOnHand?: number
-}
-
-function CreateFaction({name, type, excludes = [], corpCity = undefined, backdoorServer = undefined, hackingLevel = 0, cashOnHand = 0}: FactionOptions): Faction {
-	return {
-		name,
-		type,
-		excludes,
-		corpCity,
-		backdoorServer,
-		hackingLevel,
-		cashOnHand
-	};
-}
-
 export const FACTIONS: Faction[] = [
-	CreateFaction({ name: FACTION_NAMES.CyberSec,          type: FactionType.EarlyGame, backdoorServer: "CSEC" }),
-	CreateFaction({ name: FACTION_NAMES.TianDiHui,         type: FactionType.EarlyGame, hackingLevel: 50, cashOnHand: 1e6 }),
-	CreateFaction({ name: FACTION_NAMES.Netburners,        type: FactionType.EarlyGame, hackingLevel: 80 }),
-	CreateFaction({ name: FACTION_NAMES.ShadowsOfAnarchy,  type: FactionType.EarlyGame }),
+	new Faction({ name: FACTION_NAMES.CyberSec,          type: FactionType.EarlyGame, backdoorServer: "CSEC" }),
+	new Faction({ name: FACTION_NAMES.TianDiHui,         type: FactionType.EarlyGame, cashOnHand: 1e6, hackingLevel: 50, city: CITIES.Chongqing }), // (or NT or Ishima)
+	new Faction({ name: FACTION_NAMES.Netburners,        type: FactionType.EarlyGame, hackingLevel: 80, hacknetLevels: 100, hacknetRam: 8, hacknetCores: 4 }),
+	new Faction({ name: FACTION_NAMES.ShadowsOfAnarchy,  type: FactionType.EarlyGame, infiltration: true }),
 
-	CreateFaction({ name: FACTION_NAMES.Sector12,  type: FactionType.City, cashOnHand: 1.5e7, excludes: [FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima, FACTION_NAMES.Volhaven]                      }),
-	CreateFaction({ name: FACTION_NAMES.Chongqing, type: FactionType.City, cashOnHand: 2e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
-	CreateFaction({ name: FACTION_NAMES.NewTokyo,  type: FactionType.City, cashOnHand: 2e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
-	CreateFaction({ name: FACTION_NAMES.Ishima,    type: FactionType.City, cashOnHand: 3e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
-	CreateFaction({ name: FACTION_NAMES.Aevum,     type: FactionType.City, cashOnHand: 4e7,   excludes: [FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima, FACTION_NAMES.Volhaven]                      }),
-	CreateFaction({ name: FACTION_NAMES.Volhaven,  type: FactionType.City, cashOnHand: 5e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima] }),
+	new Faction({ name: FACTION_NAMES.Sector12,  type: FactionType.City, city: CITIES.Sector12,  cashOnHand: 1.5e7, excludes: [FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima, FACTION_NAMES.Volhaven]                      }),
+	new Faction({ name: FACTION_NAMES.Chongqing, type: FactionType.City, city: CITIES.Chongqing, cashOnHand: 2e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
+	new Faction({ name: FACTION_NAMES.NewTokyo,  type: FactionType.City, city: CITIES.NewTokyo,  cashOnHand: 2e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
+	new Faction({ name: FACTION_NAMES.Ishima,    type: FactionType.City, city: CITIES.Ishima,    cashOnHand: 3e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Volhaven]                                                }),
+	new Faction({ name: FACTION_NAMES.Aevum,     type: FactionType.City, city: CITIES.Aevum,     cashOnHand: 4e7,   excludes: [FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima, FACTION_NAMES.Volhaven]                      }),
+	new Faction({ name: FACTION_NAMES.Volhaven,  type: FactionType.City, city: CITIES.Volhaven,  cashOnHand: 5e7,   excludes: [FACTION_NAMES.Sector12, FACTION_NAMES.Aevum, FACTION_NAMES.Chongqing, FACTION_NAMES.NewTokyo, FACTION_NAMES.Ishima] }),
 
-	CreateFaction({ name: FACTION_NAMES.NiteSec,      type: FactionType.HackingGroup, backdoorServer: "avmnite-02h" }),
-	CreateFaction({ name: FACTION_NAMES.TheBlackHand, type: FactionType.HackingGroup, backdoorServer: "I.I.I.I" }),
-	CreateFaction({ name: FACTION_NAMES.BitRunners,   type: FactionType.HackingGroup, backdoorServer: "run4theh111z" }),
+	new Faction({ name: FACTION_NAMES.NiteSec,      type: FactionType.HackingGroup, backdoorServer: "avmnite-02h" }),
+	new Faction({ name: FACTION_NAMES.TheBlackHand, type: FactionType.HackingGroup, backdoorServer: "I.I.I.I" }),
+	new Faction({ name: FACTION_NAMES.BitRunners,   type: FactionType.HackingGroup, backdoorServer: "run4theh111z" }),
 
-	CreateFaction({ name: FACTION_NAMES.ECorp,                     type: FactionType.Megacorporation, corpCity: CITIES.Aevum     }),
-	CreateFaction({ name: FACTION_NAMES.MegaCorp,                  type: FactionType.Megacorporation, corpCity: CITIES.Sector12  }),
-	CreateFaction({ name: FACTION_NAMES.KuaiGongInternational,     type: FactionType.Megacorporation, corpCity: CITIES.Chongqing }),
-	CreateFaction({ name: FACTION_NAMES.FourSigma,                 type: FactionType.Megacorporation, corpCity: CITIES.Sector12  }),
-	CreateFaction({ name: FACTION_NAMES.NWO,                       type: FactionType.Megacorporation, corpCity: CITIES.Volhaven  }),
-	CreateFaction({ name: FACTION_NAMES.BladeIndustries,           type: FactionType.Megacorporation, corpCity: CITIES.Sector12  }),
-	CreateFaction({ name: FACTION_NAMES.OmniTekIncorporated,       type: FactionType.Megacorporation, corpCity: CITIES.Volhaven  }),
-	CreateFaction({ name: FACTION_NAMES.BachmanAssociates,         type: FactionType.Megacorporation, corpCity: CITIES.Aevum     }),
-	CreateFaction({ name: FACTION_NAMES.ClarkeIncorporated,        type: FactionType.Megacorporation, corpCity: CITIES.Aevum     }),
-	CreateFaction({ name: FACTION_NAMES.FulcrumSecretTechnologies, type: FactionType.Megacorporation, corpCity: CITIES.Aevum, backdoorServer: "fulcrumassets" }),
+	new Faction({ name: FACTION_NAMES.ECorp,                     type: FactionType.Megacorporation, city: CITIES.Aevum     }),
+	new Faction({ name: FACTION_NAMES.MegaCorp,                  type: FactionType.Megacorporation, city: CITIES.Sector12  }),
+	new Faction({ name: FACTION_NAMES.KuaiGongInternational,     type: FactionType.Megacorporation, city: CITIES.Chongqing }),
+	new Faction({ name: FACTION_NAMES.FourSigma,                 type: FactionType.Megacorporation, city: CITIES.Sector12  }),
+	new Faction({ name: FACTION_NAMES.NWO,                       type: FactionType.Megacorporation, city: CITIES.Volhaven  }),
+	new Faction({ name: FACTION_NAMES.BladeIndustries,           type: FactionType.Megacorporation, city: CITIES.Sector12  }),
+	new Faction({ name: FACTION_NAMES.OmniTekIncorporated,       type: FactionType.Megacorporation, city: CITIES.Volhaven  }),
+	new Faction({ name: FACTION_NAMES.BachmanAssociates,         type: FactionType.Megacorporation, city: CITIES.Aevum     }),
+	new Faction({ name: FACTION_NAMES.ClarkeIncorporated,        type: FactionType.Megacorporation, city: CITIES.Aevum     }),
+	new Faction({ name: FACTION_NAMES.FulcrumSecretTechnologies, type: FactionType.Megacorporation, city: CITIES.Aevum, backdoorServer: "fulcrumassets" }),
 
-	CreateFaction({ name: FACTION_NAMES.SlumSnakes,         type: FactionType.Criminal, cashOnHand: 1e6 }),
-	CreateFaction({ name: FACTION_NAMES.Tetrads,            type: FactionType.Criminal }),
-	CreateFaction({ name: FACTION_NAMES.Silhouette,         type: FactionType.Criminal, cashOnHand: 1.5e7 }),
-	CreateFaction({ name: FACTION_NAMES.SpeakersForTheDead, type: FactionType.Criminal }),
-	CreateFaction({ name: FACTION_NAMES.TheDarkArmy,        type: FactionType.Criminal }),
-	CreateFaction({ name: FACTION_NAMES.TheSyndicate,       type: FactionType.Criminal, cashOnHand: 1e7 }),
+	new Faction({ name: FACTION_NAMES.SlumSnakes,         type: FactionType.Criminal, combatStats: 30, karma: -9, cashOnHand: 1e6, }),
+	new Faction({ name: FACTION_NAMES.Tetrads,            type: FactionType.Criminal, combatStats: 75, karma: -18, city: CITIES.Chongqing }), // (or NT or Ishima)
+	new Faction({ name: FACTION_NAMES.Silhouette,         type: FactionType.Criminal, corpExec: true, karma: -22, cashOnHand: 1.5e7 }),
+	new Faction({ name: FACTION_NAMES.SpeakersForTheDead, type: FactionType.Criminal, karma: -45, peopleKilled: 30, hackingLevel: 100, combatStats: 300, notGovJob: true }),
+	new Faction({ name: FACTION_NAMES.TheDarkArmy,        type: FactionType.Criminal, karma: -45, peopleKilled: 5, hackingLevel: 300, combatStats: 300, notGovJob: true, city: CITIES.Chongqing }),
+	new Faction({ name: FACTION_NAMES.TheSyndicate,       type: FactionType.Criminal, karma: -90, cashOnHand: 1e7, hackingLevel: 200, combatStats: 200, notGovJob: true, city: CITIES.Aevum }), // or Sector-12
 
-	CreateFaction({ name: FACTION_NAMES.TheCovenant,  type: FactionType.Midgame }),
-	CreateFaction({ name: FACTION_NAMES.Daedalus,     type: FactionType.Midgame }),
-	CreateFaction({ name: FACTION_NAMES.Illuminati,   type: FactionType.Midgame }),
+	new Faction({ name: FACTION_NAMES.TheCovenant,  type: FactionType.Midgame, augmentations: 20, cashOnHand: 7.5e9,  hackingLevel: 850, combatStats: 850 }),
+	new Faction({ name: FACTION_NAMES.Daedalus,     type: FactionType.Midgame, augmentations: 30, cashOnHand: 1e10,   hackingLevel: 2500, combatStats: 1500, hackLevelOrCombat: true }),
+	new Faction({ name: FACTION_NAMES.Illuminati,   type: FactionType.Midgame, augmentations: 30, cashOnHand: 1.5e10, hackingLevel: 1500, combatStats: 1200 }),
 
-	CreateFaction({ name: FACTION_NAMES.Bladeburners,          type: FactionType.Endgame }),
-	CreateFaction({ name: FACTION_NAMES.ChurchOfTheMachineGod, type: FactionType.Endgame }),
+	new Faction({ name: FACTION_NAMES.Bladeburners,          type: FactionType.Endgame }),
+	new Faction({ name: FACTION_NAMES.ChurchOfTheMachineGod, type: FactionType.Endgame }),
 ];
